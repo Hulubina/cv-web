11f9e9142fe6cc04d14097b2352593313d44c2b9
Andrei Hulubina <67541787+Hulubina@users.noreply.github.com>
2021-04-27 23:25:22 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-// ResumeTemplate v1.0 ClearCareer
-// url: clearcareer.ca
-// by: Iskender Piyale-Sheard
-// website: izzydoesizzy.com
-// Created 06-27-2016
- 
 
-// Smooth Scroll Function
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
